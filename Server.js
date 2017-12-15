77a7ba1dc6311b0c310304b3207d4643c9d70a1f
Archan Patkar <archanpatkar@gmail.com>
2017-12-16 02:45:16 +0530

    Starting with P2P Communication

--- Server.js
+++ Server.js
@@ -1,2 +1,21 @@
-express = require("express");
-App = express();
+var express = require("express");
+var io = require("socket.io")();
+var app = express();
+
+app.get("/Blockchain",(req,res) => {
+ res.sendFile(__dirname + "/index.html");
+});
+
+var hserver = app.listen(8000,() => console.log("Server Started at 8000"));
+
+io.listen(hserver);
+
+io.on('connection',(socket) => {
+ console.log("User Connected")
+ socket.emit('CustomEvent');
+ socket.on('disconnect',() => console.log("User Disconnected"));
+ socket.on('getData',() => {
+   console.log("getData Called");
+   socket.emit('setData',"Data from some DB");
+ });
+});
